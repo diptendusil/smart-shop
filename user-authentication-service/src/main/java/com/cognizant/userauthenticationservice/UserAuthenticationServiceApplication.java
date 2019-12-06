5c15abf509c574bc777f9a5911a37d6530d622b1
Khan, Nishat (Cognizant) <nishat.khan2@cognizant.com>
2019-12-06 17:13:11 +0530

    Fixed I guess

---
+++ user-authentication-service/src/main/java/com/cognizant/userauthenticationservice/UserAuthenticationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cognizant.userauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationServiceApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(UserAuthenticationServiceApplication.class, args);
+	}
+
+}
