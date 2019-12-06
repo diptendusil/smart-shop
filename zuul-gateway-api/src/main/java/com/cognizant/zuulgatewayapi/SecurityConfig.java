7932b53bf9b896102b90e7cc4a6a6ab8d9127fe1
Diptendu Sil <diptendu.sil@cognizant.com>
2019-12-06 12:55:54 +0530

    Hibernate properties

---
+++ zuul-gateway-api/src/main/java/com/cognizant/zuulgatewayapi/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.cognizant.zuulgatewayapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication().withUser(users.username("John").password("t1").roles("Employee"))
+				.withUser(users.username("Mary").password("t2").roles("Manager"))
+				.withUser(users.username("Susan").password("t3").roles("Admin"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().httpBasic().and().authorizeRequests().antMatchers("/authenticate").permitAll();
+	}
+}
