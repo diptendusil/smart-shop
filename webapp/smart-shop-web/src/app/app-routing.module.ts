a52b37dad0f4be9cf14162a4eaee56864ba136b7
Khan, Nishat (Cognizant) <nishat.khan2@cognizant.com>
2019-12-06 12:20:50 +0530

    Added Login and signup component with routes

--- webapp/smart-shop-web/src/app/app-routing.module.ts
+++ webapp/smart-shop-web/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './site/login/login.component';
+import { SignUpComponent } from './site/sign-up/sign-up.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
