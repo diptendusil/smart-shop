42ead4019752fdb040a04c28716535acd614fe6f
Ghose <810491@cognizant.com>
2019-12-06 09:53:17 +0530

    added eureka server, zuul, auth service

---
+++ zuul-gateway-api/src/main/java/com/cognizant/zuulgatewayapi/ZuulGatewayApiApplication.java
@@ -0,0 +1,19 @@
+package com.cognizant.zuulgatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableZuulProxy
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class ZuulGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApiApplication.class, args);
+	}
+
+}
---
+++ zuul-gateway-api/src/test/java/com/cognizant/zuulgatewayapi/ZuulGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.zuulgatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
