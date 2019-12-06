7eef1be7581d09bbbf9de5852a466917dbee6d50
Diptendu Sil <diptendu.sil@cognizant.com>
2019-12-06 12:36:51 +0530

    SQL creation and insertion

---
+++ dbscripts/smart-shop-creation-schema.sql
@@ -0,0 +1,12 @@
+use `smart-shop`;
+
+-- role table
+INSERT INTO `smart-shop`.`role` (`ro_id`, `ro_name`) VALUES ('U', 'ROLE_USER');
+INSERT INTO `smart-shop`.`role` (`ro_id`, `ro_name`) VALUES ('M', 'ROLE_MANAGER');
+INSERT INTO `smart-shop`.`role` (`ro_id`, `ro_name`) VALUES ('A', 'ROLE_ADMIN');
+INSERT INTO `smart-shop`.`role` (`ro_id`, `ro_name`) VALUES ('S', 'ROLE_SUPER_USER');
+
+
+-- user table
+INSERT INTO `smart-shop`.`user` (`us_id`, `us_first_name`, `us_last_name`, `us_age`, `us_gender`, `us_contact`, `us_password`, `us_status`, `us_secret_question_1`, `us_secret_answer_1`, `us_secret_question_2`, `us_secret_answer_2`, `us_secret_question_3`, `us_secret_answer_3`, `us_ro_id`)
+VALUES ('admin', 'Admin', 'Admin', '20', 'M', '1234567890', '$2y$10$rtKgYrFaR22GMGtNbmNI8eUwmWzWi/KBNysNA9hDbNB8tHct5eHgm', 'A', '1st letter', 'A', '2nd letter', 'B', '3rd letter', 'C', 'S');
