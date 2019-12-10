b389f3558f874559395cc6052841a7a14336514f
Diptendu Sil <diptendu.sil@cognizant.com>
2019-12-10 12:41:17 +0530

    Nothing

--- user-authentication-service/src/main/java/com/cognizant/userauthenticationservice/repositories/UserRepository.java
+++ user-authentication-service/src/main/java/com/cognizant/userauthenticationservice/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.cognizant.userauthenticationservice.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cognizant.userauthenticationservice.entities.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
-
+	
+	@Query("")
+	public List<User> getAllManagers();
 }
