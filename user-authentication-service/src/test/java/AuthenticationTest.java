ffdaf97212a8de9f3a18d634186cbde9fd6142c3
Amit Kumar4 <amit.kumar360ad5@cognizant.com>
2019-12-16 17:55:18 +0530

    amit

--- user-authentication-service/src/test/java/AuthenticationTest.java
+++ user-authentication-service/src/test/java/AuthenticationTest.java
@@ -1,17 +1,67 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 
 
 class AuthenticationTest {
+	private BasicAuthenticationFilter filter;
+    private AuthenticationManager manager;
+    
+    @BeforeEach
+    void setUp() throws Exception {
+                SecurityContextHolder.clearContext();
+                UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(
+                                        "user", "user");
+                authRequest.setDetails(new WebAuthenticationDetails(new MockHttpServletRequest()));
+                Authentication authentication=new UsernamePasswordAuthenticationToken("user", "user", 
+                                        AuthorityUtils.createAuthorityList("ROLE_USER"));
+                manager=mock(AuthenticationManager.class);
+                when(manager.authenticate(authRequest)).thenReturn(authentication);
+                //when(manager.authenticate(not(eq(authRequest)))).thenThrow(new BadCredentialsException(""));
+                filter=new BasicAuthenticationFilter(manager);
+    }
+    
 
-	
-	
+
+
+	@AfterEach
+	void clearContext()
+	{
+		SecurityContextHolder.clearContext();
+	}
 	
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void testFilterIgnoresRequestContainingNoAutherizationHeader() throws ServletException, IOException {
+        MockHttpServletRequest request=new MockHttpServletRequest();
+        request.setServletPath("/authenticate");
+        final MockHttpServletResponse response=new MockHttpServletResponse();
+        FilterChain chain=mock(FilterChain.class);
+        filter.doFilter(request, response, chain);
+        verify(chain).doFilter(request,response);
+        assertThat(SecurityContextHolder.getContext().getAuthentication()).isNull();
+}
 
+	
+	
 }
