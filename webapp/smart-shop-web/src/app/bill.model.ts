0a5ea951542b936c2346876d75517370c2fce273
Diptendu Sil <diptendu.sil@cognizant.com>
2019-12-16 10:07:41 +0530

    bill model

---
+++ webapp/smart-shop-web/src/app/bill.model.ts
@@ -0,0 +1,18 @@
+import { User } from './site/user';
+import { Product } from './product/product.model';
+
+export interface Bill {
+    billId: number; //add GeneratedValue in java
+    user: User; //many-one
+    total: number;
+    rewardPoints: number;
+    dateOfPurchase: Date;
+    purchaseItems: PurchaseItem[]; //one-many
+}
+
+export interface PurchaseItem {
+    purchaseId: number; //add GeneratedValue in java
+    product: Product; //one-one
+    quantity: number;
+    price: number;
+}
