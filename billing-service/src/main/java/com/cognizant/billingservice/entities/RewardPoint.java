bf97eeccccf3f4679ca618a5778ba084c3aa29a8
Khan, Nishat (Cognizant) <nishat.khan2@cognizant.com>
2019-12-17 10:38:45 +0530

    Fixed issue

--- billing-service/src/main/java/com/cognizant/billingservice/entities/RewardPoint.java
+++ billing-service/src/main/java/com/cognizant/billingservice/entities/RewardPoint.java
@@ -1,5 +1,7 @@
 package com.cognizant.billingservice.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,7 +11,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="reward_point")
-public class RewardPoint {
+public class RewardPoint implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	@Id
 	@OneToOne
 	@JoinColumn(name="rp_us_id")
