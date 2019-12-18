2356c26ae18331dda247af14ac6f09b0b58cb57f
Khan, Nishat (Cognizant) <nishat.khan2@cognizant.com>
2019-12-18 12:56:09 +0530

    Added add offer

--- webapp/smart-shop-web/src/app/shop-manager/manage-offers/manage-offers.component.ts
+++ webapp/smart-shop-web/src/app/shop-manager/manage-offers/manage-offers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Offer } from 'src/app/product/product.model';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Offer, Product } from 'src/app/product/product.model';
 import { OfferService } from 'src/app/services/offer.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-manage-offers',
@@ -10,14 +11,111 @@ import { OfferService } from 'src/app/services/offer.service';
 })
 export class ManageOffersComponent implements OnInit {
   filterBy: FormControl = new FormControl('');
-  sortBy: FormControl = new FormControl('');
-  today: string = new Date().toISOString().slice(0,10);
+  filterValue: FormControl = new FormControl('');
+  sortBy: FormControl = new FormControl('offerId');
+  sortOrder: FormControl = new FormControl('asc');
+  today: string = new Date().toISOString().slice(0, 10);
+  addForm: FormGroup;
+  allProducts: Product[];
   allOffers: Offer[];
-  constructor(private offerService: OfferService) {
-    this.offerService.getAllOffers().subscribe(offers => this.allOffers = offers);
+  filteredOffers: Offer[];
+  constructor(private offerService: OfferService, private formBuilder: FormBuilder, private productService: ProductService) {
+    this.offerService.getAllOffersAdmin().subscribe(offers => {
+      this.allOffers = offers;
+      this.filteredOffers = offers;
+      console.log(this.allOffers);
+    });
+    this.productService.getAllProducts().subscribe(products => {
+      console.log(products);
+      
+      this.allProducts = products;
+      this.addForm = this.formBuilder.group({
+        offerName: ['', Validators.required],
+        offerDate: ['',Validators.required],
+        productCode: ['', [Validators.required, this.checkProduct.bind(this)]],
+        discountRate: ['', Validators.required]
+      });
+    });
+    this.filterBy.valueChanges.subscribe(() => {
+      this.filteredOffers = this.allOffers;
+      this.filterValue.setValue(null);
+    });
+    this.filterValue.valueChanges.subscribe((value: string) => {
+      if (value) {
+        if (this.filterBy.value === 'productCode') {
+          this.filteredOffers = this.allOffers.filter(offer => offer.productCode.toLowerCase().includes(value.toLowerCase()));
+        } else if (this.filterBy.value === 'productName') {
+          this.filteredOffers = this.allOffers.filter(offer => offer.productName.toLowerCase().includes(value.toLowerCase()));
+        } else if (this.filterBy.value === 'discountRate') {
+          this.filteredOffers = this.allOffers.filter(offer => offer.discountRate === +value);
+        } else if (this.filterBy.value === 'offerName') {
+          this.filteredOffers = this.allOffers.filter(offer => offer.offerName.toLowerCase().includes(value.toLowerCase()))
+        }
+      } else {
+        this.filteredOffers = this.allOffers;
+      }
+    });
   }
 
   ngOnInit() {
-  }
+    this.sortOrder.valueChanges.subscribe((value: string) => {
+      this.sort(value);
+    });
+    this.sortBy.valueChanges.subscribe(() => {
+      this.sort(this.sortOrder.value);
+    });
 
+  }
+  sort(order: string) {
+    this.filteredOffers = this.filteredOffers.sort(this.comparator);
+    if (order === 'dsc') {
+      this.filteredOffers = this.filteredOffers.reverse();
+    }
+  }
+  compare(a, b) {
+    return a < b ? -1 : a > b ? 1 : 0;
+  }
+  comparator = (a: Offer, b: Offer) => {
+    const param = this.sortBy.value;
+    if (param === 'product') {
+      const currentProductName = a.productName;
+      const nextProductName = b.productName;
+      return this.compare(currentProductName, nextProductName);
+    } else {
+      return this.compare(a[param], b[param]);
+    }
+  }
+  checkDate() {
+    return this.filterBy.value !== 'offerDate';
+  }
+  get offerName() {
+    return this.addForm.get('offerName');
+  }
+  get offerDate() {
+    return this.addForm.get('offerDate');
+  }
+  get productCode() {
+    return this.addForm.get('productCode');
+  }
+  get discountRate() {
+    return this.addForm.get('discountRate');
+  }
+  checkProduct(productControl: FormControl): { [s: string]: boolean } {
+    if(!this.allProducts.find(product => product.productCode === productControl.value)) {
+      return {'noMatch': true}
+    }
+  }
+  addOffer() {
+    if(this.addForm.valid) {
+      const newOffer: Offer = {
+        offerName: this.offerName.value,
+        offerDate: new Date(this.offerDate.value),
+        discountRate: this.discountRate.value,
+        productCode:  this.productCode.value
+      };
+      this.offerService.addOffer(newOffer).subscribe(offer => {
+        this.allOffers.push(offer);
+      })
+    }
+  }
 }
