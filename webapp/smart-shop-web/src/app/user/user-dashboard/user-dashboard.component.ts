dab95979b1c330075c2835d52b23c2cf7b7cb801
Khan, Nishat (Cognizant) <nishat.khan2@cognizant.com>
2019-12-20 10:00:17 +0530

    fixes

--- webapp/smart-shop-web/src/app/user/user-dashboard/user-dashboard.component.ts
+++ webapp/smart-shop-web/src/app/user/user-dashboard/user-dashboard.component.ts
@@ -1,6 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
-import { ItemInfoComponent } from 'src/app/product/item-info/item-info.component';
 import { Category, Offer, Product } from 'src/app/product/product.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { OfferService } from 'src/app/services/offer.service';
@@ -21,8 +20,8 @@ export class UserDashboardComponent implements OnInit {
   ngOnInit() {
     this.productService.getAllCategories().subscribe(categories => this.categories = categories);
     this.offerService.getAllOffers().subscribe(offers => this.offers = offers);
-    this.productService.getAllProductsInStock().subscribe(products => this.allProducts = products.slice(0,6));
-    if(this.authService.loggedInUser.value) {
+    this.productService.getAllProductsInStock().subscribe(products => this.allProducts = products.slice(0, 6));
+    if (this.authService.loggedInUser.value) {
       this.authService.loggedInUser.pipe(switchMap(user => this.productService.getSuggestions(user.userId))).subscribe((suggestions) => {
         this.suggestions = suggestions;
         console.log(suggestions);
