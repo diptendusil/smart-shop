32f0c4e13e03747ead2687587e99f37b972a711e
Diptendu Sil <diptendu.sil@cognizant.com>
2019-12-21 15:13:34 +0530

    ui improvement v1.2

--- webapp/smart-shop-web/src/app/site/header/header.component.ts
+++ webapp/smart-shop-web/src/app/site/header/header.component.ts
@@ -3,6 +3,8 @@ import { User } from '../user';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { dashboardUrl } from '../user-navigation-handler';
+import { Product } from 'src/app/product/product.model';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-header',
@@ -11,7 +13,10 @@ import { dashboardUrl } from '../user-navigation-handler';
 })
 export class HeaderComponent implements OnInit {
   loggedInUser: User = null;
-  constructor(private router: Router, private authService: AuthService) { }
+  productsLoaded: boolean = false;
+  allProducts: Product[];
+  filteredProducts: Product[];
+  constructor(private router: Router, private authService: AuthService, private productService: ProductService) { }
 
   ngOnInit() {
     this.authService.loggedInUser.subscribe(user => this.loggedInUser = user);
@@ -22,12 +27,25 @@ export class HeaderComponent implements OnInit {
   }
 
   home() {
-    if(this.authService.loggedInUser.value) {
+    if (this.authService.loggedInUser.value) {
       const redirectUrl = dashboardUrl(this.authService.loggedInUser.value);
       this.router.navigate(redirectUrl);
     }
     else {
       this.router.navigate(["/"]);
+    }
+  }
+
+  loadProducts() {
+    if(!this.productsLoaded) {
+      this.productService.getAllProductsInStock().subscribe((products) => {
+        console.log("Loaded");
+        this.allProducts = [...products];
+        this.filteredProducts = [...products];
+        this.productsLoaded = true;
+      }, () => {
+        console.log("Could not load products");
+      })
     }
   }
 }
