4a776b144acff5902a31f65ee9ef9944eb9ca385
Diptendu Sil <diptendu.sil@cognizant.com>
2019-12-21 17:17:09 +0530

    ui improvements v1.3

--- webapp/smart-shop-web/src/app/site/header/header.component.ts
+++ webapp/smart-shop-web/src/app/site/header/header.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from 'src/app/services/auth.service';
 import { dashboardUrl } from '../user-navigation-handler';
 import { Product } from 'src/app/product/product.model';
 import { ProductService } from 'src/app/services/product.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -16,6 +17,10 @@ export class HeaderComponent implements OnInit {
   productsLoaded: boolean = false;
   allProducts: Product[];
   filteredProducts: Product[];
+  autoComplete:boolean = false;
+
+  searchHistory: {code: string, name: string}[];
+  search: FormControl = new FormControl('');
   constructor(private router: Router, private authService: AuthService, private productService: ProductService) { }
 
   ngOnInit() {
@@ -47,5 +52,29 @@ export class HeaderComponent implements OnInit {
         console.log("Could not load products");
       })
     }
+  }
+
+  loadDropDown() {
+    if(this.search.value.length > 0) {
+      const filter = this.allProducts.filter((product) => {
+        return (product.productName + " " + product.brand).toLowerCase().includes(this.search.value.toLowerCase());
+      });
+      this.filteredProducts = [...filter];
+      this.autoComplete = true;
+    }
+    else {
+      this.filteredProducts = [...this.allProducts];
+      this.autoComplete = false;
+    }
+  }
+
+  resetSearch() {
+    this.autoComplete = false;
+    this.search.setValue('');
+    this.filteredProducts = [...this.allProducts];
+  }
+
+  closeDrop() {
+    this.autoComplete = false;
   }
 }
